6741aa17ea264878b312de43f6aaee408d46e923
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-07-16 13:49:15 +0700

    loginUser, registerUser, registerMotherProfile

--- raymondtes/controllers/controller.js
+++ raymondtes/controllers/controller.js
@@ -1,3 +1,5 @@
+const { comparePassword } = require("../helpers/bcrypt");
+const { signToken } = require("../helpers/jwt");
 const {
   User,
   MotherProfile,
@@ -7,10 +9,123 @@ const {
 } = require("../models");
 
 class Controller {
+  static async loginUser(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email) {
+        throw { name: "EmailRequired" };
+      }
+      if (!password) {
+        throw { name: "PasswordRequired" };
+      }
+
+      const foundUser = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!foundUser) {
+        throw { name: "InvalidLogin" };
+      }
+
+      const isMatched = comparePassword(password, foundUser.password);
+
+      if (!isMatched) {
+        throw { name: "InvalidLogin" };
+      }
+
+      const payload = {
+        id: foundUser.id,
+        role: foundUser.role,
+      };
+
+      console.log(payload);
+
+      const access_token = signToken(payload);
+      res.status(200).json({ access_token });
+    } catch (err) {
+      if (err.name == "PasswordRequired") {
+        res.status(400).json({ message: "Password is required" });
+      } else if (err.name == "EmailRequired") {
+        res.status(400).json({ message: "Email is required" });
+      } else if (err.name == "InvalidLogin") {
+        res.status(401).json({ message: "Invalid email/password" });
+      } else {
+        res.status(500).json(err);
+      }
+    }
+  }
+
+  static async registerUser(req, res) {
+    try {
+      const role = "Admin";
+      const { username, password, email, noRT } = req.body;
+
+      const createdUser = await User.create({
+        username,
+        password,
+        email,
+        noRT,
+        role,
+      });
+
+      res.status(201).json({
+        id: createdUser.id,
+        email: createdUser.email,
+        username: createdUser.username,
+        noRT: createdUser.noRT,
+      });
+    } catch (err) {
+      if (
+        err.name == "SequelizeUniqueConstraintError" ||
+        err.name == "SequelizeValidationError"
+      ) {
+        res.status(400).json({ message: err.errors[0].message });
+      } else {
+        res.status(500).json(err);
+      }
+    }
+  }
+
+  static async registerMotherProfile(req, res) {
+    try {
+      const UserId = req.user.id;
+      const { name, NIK, password, address } = req.body;
+
+      const createdMotherProfile = await MotherProfile.create({
+        UserId,
+        name,
+        NIK,
+        password,
+        address,
+      });
+
+      res.status(201).json({
+        id: createdMotherProfile.id,
+        name: createdMotherProfile.name,
+        NIK: createdMotherProfile.NIK,
+        address: createdMotherProfile.address,
+      });
+    } catch (err) {
+      if (
+        err.name == "SequelizeUniqueConstraintError" ||
+        err.name == "SequelizeValidationError"
+      ) {
+        res.status(400).json({ message: err.errors[0].message });
+      } else {
+        res.status(500).json(err);
+      }
+    }
+  }
+
   static async fetchPregnancyData(req, res) {
     // res.send("masok");
     try {
       const { id } = req.params;
+      // console.log("masok <<<<<<<<");
+      // console.log(req.user);
 
       const data = await Pregnancy.findOne({
         where: {
@@ -31,11 +146,11 @@ class Controller {
         tempSelisihArr.push(beratBulananArr[i + 1] - beratBulananArr[i]);
       }
 
-      console.log(tempSelisihArr);
+      // console.log(tempSelisihArr);
       //   data.PregnancyDatum.selisihBulanan = tempSelisihArr;
       //   console.log(data);
 
-      res.status(200).json({ data, tempSelisihArr });
+      res.status(200).json({ data, selisihBulanan: tempSelisihArr });
     } catch (err) {
       res.status(500).json(err);
     }
--- raymondtes/routes/index.js
+++ raymondtes/routes/index.js
@@ -1,7 +1,20 @@
 const Controller = require("../controllers/controller");
+const authentication = require("../middlewares/authentication");
+const authorizationAdmin = require("../middlewares/authorizationAdmin");
+const authorizationSuperAdmin = require("../middlewares/authorizationSuperAdmin");
 
 const router = require("express").Router();
 
+router.post("/login", Controller.loginUser);
+
+router.use(authentication);
+
+router.post("/registerUser", authorizationSuperAdmin, Controller.registerUser);
+router.post(
+  "/registerMotherProfile",
+  authorizationAdmin,
+  Controller.registerMotherProfile
+);
 router.get("/detailpregnancy/:id", Controller.fetchPregnancyData);
 
 module.exports = router;
