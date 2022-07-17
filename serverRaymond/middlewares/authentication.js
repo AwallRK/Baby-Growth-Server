ea029c34e8d3f69836a64f90e86b67e8c95c7177
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-07-17 18:25:59 +0700

    branch

---
+++ serverRaymond/middlewares/authentication.js
@@ -0,0 +1,40 @@
+const { verifyToken } = require("../helpers/jwt");
+const { User } = require("../models");
+
+const authentication = async (req, res, next) => {
+  try {
+    const access_token = req.headers.access_token;
+    // console.log("masok <<<<<<<<<<<<<<<<<<<");
+    // console.log(access_token);
+    if (!access_token) {
+      throw { name: "InvalidToken" };
+    }
+
+    const payload = verifyToken(access_token);
+    // console.log(payload);
+    const foundUser = await User.findOne({
+      where: {
+        id: payload.id,
+      },
+    });
+    // console.log(foundUser);
+    if (!foundUser) {
+      throw { name: "InvalidToken" };
+    }
+    req.user = {
+      id: payload.id,
+      role: payload.role,
+    };
+    console.log(req.user);
+    // console.log(payload.id);
+    next();
+  } catch (err) {
+    if (err.name == "InvalidToken" || err.name == "JsonWebTokenError") {
+      res.status(401).json({ message: "Invalid token" });
+    } else {
+      res.status(500).json(err);
+    }
+  }
+};
+
+module.exports = authentication;
---
+++ serverRaymond/models/babydata.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class BabyData extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      BabyData.belongsTo(models.Pregnancy);
+    }
+  }
+  BabyData.init(
+    {
+      PregnancyId: DataTypes.INTEGER,
+      beratAwal: DataTypes.FLOAT,
+      beratBulanan: DataTypes.STRING,
+      tanggalDicatat: DataTypes.DATE,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "BabyData",
+    }
+  );
+  return BabyData;
+};
