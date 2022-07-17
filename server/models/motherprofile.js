6bfe08ded99d91663c4d9a658079e98b5889ba5d
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-07-17 21:46:25 +0700

    add latitude longitude

--- server/models/motherprofile.js
+++ server/models/motherprofile.js
@@ -42,6 +42,8 @@ module.exports = (sequelize, DataTypes) => {
       },
       address: DataTypes.STRING,
       UserId: DataTypes.INTEGER,
+      latitude: DataTypes.DECIMAL,
+      longitude: DataTypes.DECIMAL,
     },
     {
       sequelize,
